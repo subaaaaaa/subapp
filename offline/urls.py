dc35858d040712b506e272ee2b9c25d219fb647e
subaaa <suba1124@gmail.com>
2018-01-06 17:28:55 +0000

    Form

--- offline/urls.py
+++ offline/urls.py
@@ -6,6 +6,6 @@ from offline.views import AddView
 urlpatterns = [
     url(r'^$', views.customer_list, name='offline/customer_list'),
     url(r'^customer_uploaded_list$', views.customer_uploaded_list, name='offline/customer_uploaded_list'),
-    url(r'^customer_sync$', AddView.as_view()),
+    url(r'^customer_sync$', AddView.as_view(), name='offline/customer_sync'),
     url(r'^sw.js', (TemplateView.as_view(template_name="sw.js", content_type='application/javascript', )), name='sw.js'),
 ]
