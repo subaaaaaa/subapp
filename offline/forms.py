b9747dcd2d39e62b6dcca739016dae5c5aff3e9c
subaaa <suba1124@gmail.com>
2018-01-07 15:33:43 +0000

    Design

--- offline/forms.py
+++ offline/forms.py
@@ -7,7 +7,16 @@ class LeadSyncForm(forms.ModelForm):
         model = Lead
         #fields = '__all__'
         fields = ('lno', 'jname', 'kname', 'tel', 'postal', 'address1', 'address2', 'address3')
- 
+        widgets = {
+            'lno': forms.TextInput(attrs={'class': 'form-control'}),
+            'jname': forms.TextInput(attrs={'class': 'form-control'}),
+            'kname': forms.TextInput(attrs={'class': 'form-control'}),
+            'tel': forms.TextInput(attrs={'class': 'form-control'}),
+            'postal': forms.TextInput(attrs={'class': 'form-control'}),
+            'address1': forms.TextInput(attrs={'class': 'form-control'}),
+            'address2': forms.TextInput(attrs={'class': 'form-control'}),
+            'address3': forms.TextInput(attrs={'class': 'form-control'}),
+        } 
 # ここが追加!!
 LeadFormSet = forms.modelformset_factory(Lead, form=LeadSyncForm, extra=100, can_delete=True)
 
